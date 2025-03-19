9bf6c31e634ac9f49d884aefb8f870ff36aa1764
JorisLespagnol <46922441+Bwerpophile@users.noreply.github.com.>
2025-03-19 13:01:49 +0100

    Create the articles controller and show view and form and edit view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"  
-  get "up" => "rails/health#show", as: :rails_health_check
-
+  
+  resources :articles
 end
